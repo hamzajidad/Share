8363172d0a6b5bfa1aefac56d0848675010baf09
Adrien HOURY <adrien.houry@gmail.com>
2016-05-02 21:54:25 +0200

    remove some parasits

--- newVersion/RMI-Server/src/HelloInterface.java
+++
@@ -1,9 +0,0 @@
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface HelloInterface extends Remote{
-
-	public String hello() throws RemoteException;
-	public int Calcul(int a, int b) throws RemoteException;
-	
-}
